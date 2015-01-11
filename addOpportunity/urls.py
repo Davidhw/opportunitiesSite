6b742b60f4ae78b472375f6b8782bf8644e992b4
davidhw <davidweinstein@ncf.edu>
2015-01-10 23:09:51 -0500

    Adding the files used to make the project publicly accessible on appspot. Also, made templates extend a base template to simplify adding a navigation bar to the top of all pages.

--- addOpportunity/urls.py
+++ addOpportunity/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from addOpportunity import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.add),
+    url(r'^$', views.add, name = "add"),
 #    url(r'^viewAdded', views.IndexView.as_view(), name='index'),
 #    url(r'^post/form_upload.html', views.add, name='post_form_upload'),
 
--- opportunitiesSite/urls.py
+++ opportunitiesSite/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from addOpportunity import views
-
+from addOpportunity import views as views2
+import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,11 +9,12 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 # testing, remove next line later
 #    url(r'post/form_upload.html', views.IndexView.as_view()), 
+    url('^$',views.HomeView.as_view(), name='home'),
     url(r'^add/',include('addOpportunity.urls')),
     url(r'^addOpportunity/',include('addOpportunity.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^viewAdded/(?P<pk>\d+)', views.DetailView.as_view(), name='detail'),
-    url(r'^viewAdded/', views.IndexView.as_view(), name='index'),
+    url(r'^viewAdded/(?P<pk>\d+)', views2.DetailView.as_view(), name='detail'),
+    url(r'^viewAdded/', views2.IndexView.as_view(), name='index'),
     url(r'^post/form_upload.html$',
         'addOpportunity.views.add', name = 'post_form_upload'),
 )
